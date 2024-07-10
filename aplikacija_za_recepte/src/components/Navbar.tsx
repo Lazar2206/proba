de5f2ba2f2667e178fb400d1c30bd27d2d3ba166
jagosmedenica <jagos.medenica.mne@gmail.com>
2024-07-10 20:00:50 +0200

    treci commit-Navbar

---
+++ aplikacija_za_recepte/src/components/Navbar.tsx
@@ -0,0 +1,64 @@
+import { Link, NavLink } from 'react-router-dom';
+
+import '../styles/Navbar.css';
+import { FaFacebookF, FaInstagram, FaTiktok } from 'react-icons/fa';
+
+const Navbar = () => {
+  const toggleMenu = () => {};
+
+  return (
+    <header className='row container'>
+      <Link to={'/'} className='logo lobsterFont'>
+        TacoTime
+      </Link>
+      <div className='toggleMenu' onClick={toggleMenu}></div>
+      <nav className='navigation row'>
+        <ul className='primary-links row'>
+          <li>
+            <NavLink
+              to={'/'}
+              className={({ isActive }) => (isActive ? 'active' : '')}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={'/recipes'}
+              className={({ isActive }) => (isActive ? 'active' : '')}
+            >
+              Recipes
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={'/about'}
+              className={({ isActive }) => (isActive ? 'active' : '')}
+            >
+              About us
+            </NavLink>
+          </li>
+        </ul>
+        <ul className='secondary-links row'>
+          <li>
+            <Link to={'/'}>
+              <FaFacebookF />
+            </Link>
+          </li>
+          <li>
+            <Link to={'/'}>
+              <FaInstagram />
+            </Link>
+          </li>
+          <li>
+            <Link to={'/'}>
+              <FaTiktok />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
