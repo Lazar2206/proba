55e66dde93833ae0ec20069472830a9e998d2479
lakicevicteodora <dora.26.12@gmail.com>
2024-07-13 17:14:53 +0200

    assets for Contact Us

--- aplikacija_za_recepte/src/pages/Home.tsx
+++ aplikacija_za_recepte/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import Categories from '../components/Home/Categories';
-import FeaturedRecipes from '../components/Home/FeaturedRecipes';
+import FeaturedRecipes from '../components/Home/Featuredrecipes';
 import Hero from '../components/Home/Hero';
 
 const Home = () => {
