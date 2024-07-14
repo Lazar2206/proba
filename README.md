89e8bed6af0a762079418023a2340c95e383a54b
lazar2206 <lazarjovano@gmail.com>
2024-07-14 08:37:42 +0200

    Contact us form fields

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# proba
+# Uputstvo 
+preuzeti sajt sa github-a
+otvoriti visual studio i kliknuti open folder, izabrati folder proba
+otvoriti terminal i ukucati cd aplikacija_za_recepte
+potom uneti npm install
+u terminalu ukucati npm run dev da bi pokrenuli aplikaciju
+ctrl+click na localhost:5173 što će vam izbaciti u terminalu
--- aplikacija_za_recepte/src/pages/Contact.tsx
+++ aplikacija_za_recepte/src/pages/Contact.tsx
@@ -1,8 +1,19 @@
 import lizard from '../assets/lizard.png';
 import envelope from '../assets/envelope.png';
 import '../styles/Contact.css';
+import { useState } from 'react';
 
 const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSendMessage = () => {
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+
   return (
     <section className='contact'>
       <div className='container row'>
@@ -14,10 +25,28 @@ const Contact = () => {
           <div className='content'>
             <div className='recipe row'>
               <h2>Contact us</h2>
-              <input type='text' placeholder='Name' />
-              <input type='email' placeholder='Email' />
-              <input type='text' placeholder='Message' className='textbox' />
-              <button className='btn'>Send Message</button>
+              <input
+                type='text'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder='Name'
+              />
+              <input
+                type='email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder='Email'
+              />
+              <input
+                type='text'
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                placeholder='Message'
+                className='textbox'
+              />
+              <button onClick={handleSendMessage} type='button' className='btn'>
+                Send Message
+              </button>
             </div>
           </div>
         </div>
